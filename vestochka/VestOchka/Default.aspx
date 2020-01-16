f496fbba3a374d6584abad5ebb983ce286eef966
Tysha1337 <thedreamkill@gmail.com>
2020-01-16 22:37:27 +0700

    Изменен основной проект

---
+++ vestochka/VestOchka/Default.aspx
@@ -0,0 +1,11 @@
+﻿<%@ Page Title="Home Page" Language="C#" MasterPageFile="~/Site.Master" AutoEventWireup="true" CodeBehind="Default.aspx.cs" Inherits="VestOchka._Default" %>
+
+<asp:Content ID="BodyContent" ContentPlaceHolderID="MainContent" runat="server">
+    <div>
+
+        <asp:Table ID="Table1" runat="server" BorderColor="#99FF66" BorderStyle="Solid" GridLines="Horizontal" Height="284px" Width="1530px">
+        </asp:Table>
+
+    </div>
+
+</asp:Content>
---
+++ vestochka/VestOchka/User.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace VestOchka
+{
+    public class User
+    {
+        private Position _Position;
+        public User(Position Position)
+        {
+            if (Position != null)
+            {
+                _Position = Position;
+            }
+            else
+            {   
+                throw new Exception("Пустое значение должности пользователя");
+            }
+        }
+        public User(): this(null){ }
+
+    }
+}
